c1f62a076328130994f4a181452e3523b7862714
sharpel malak <62040665+sharpelmalak@users.noreply.github.com>
2023-04-10 16:28:02 +0200

    Initial commit

---
+++ MovingCarProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * MovingCarProject.c
+ *
+ * Created: 4/10/2023 4:18:34 PM
+ * Author : sharb
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# MovingCarProject
+ a four-driving wheel robot, the system is designed so that the car moves in a rectangular shape.
